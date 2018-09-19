ec424f3958e0b1fa91ec861de80b53fd29268420
Ghazarios <samreid.manez@gmail.com>
2018-09-19 12:01:59 +0000

    Added Stripe payment and form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :payment
 end
+
